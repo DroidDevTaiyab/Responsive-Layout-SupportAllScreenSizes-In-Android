7cd5fb80b50038df08837ccd87f1fc4e90417cd3
ali <taiyabalispn@gmail.com>
2021-04-08 22:53:58 +0530

    implementation done.

---
+++ app/src/main/java/com/techpassmaster/responsivedesignlayout/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.techpassmaster.responsivedesignlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Created by Techpass Master on 8-April-21.
+ * Website - https://techpassmaster.com/
+ * Email id - hello@techpassmaster.com
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
